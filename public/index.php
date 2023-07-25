2eadc0b341a9a5664d74d5e359f978dbef0775f8
zahirul10 <70477227+zahirul10@users.noreply.github.com>
2023-07-25 10:34:50 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ define('LARAVEL_START', microtime(true));
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
-| Hello there !
+| Hello there ! QA here
 | If the application is in maintenance / demo mode via the "down" command
 | we will load this file so that any pre-rendered content can be shown
 | instead of starting the framework, which could cause an exception.
